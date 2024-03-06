83220a1b858f64d478e17a415e36fb1d18ba091b
Mike Warren <mjwarren3@gmail.com>
2024-03-06 09:23:06 -0500

    Switched to Link from a href

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import PhoneLayout from '@/layouts/layout'
 import { Camera, Map, NotepadText, Phone } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const apps = [
@@ -26,12 +27,12 @@ function page() {
         <PhoneLayout>
             <div className="grid grid-cols-4 gap-6 px-4 py-12">
                 {apps.map((app) => (
-                    <a href={app.href} key={app.name}>
+                    <Link href={app.href} key={app.name}>
                         <div className="h-16 w-16 flex flex-col shadow-[0_0_20px_-15px_rgba(0,0,0,1)] rounded-2xl justify-center items-center text-slate-700">
                             {app.icon}
                         </div>
                         <p className="ml-2 mt-1 text-slate-700 text-center font-medium text-xs">{app.name}</p>
-                    </a>
+                    </Link>
                 ))}
             </div>
         </PhoneLayout>
