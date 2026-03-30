2ee79e37e68d003464e05a7af28e0b316f591760
Mike Warren <mikewarren@Mikes-MacBook-Pro.local>
2026-03-30 13:34:36 -0400

    Fixed plausible implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,6 @@
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Script
-        async
-        strategy="afterInteractive"
-        src="https://plausible.io/js/pa-glsletFAv3Li7iWWsD06g.js"
-      />
-      <Script id="plausible-init" strategy="afterInteractive">
-        {`window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init();`}
-      </Script>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://plausible.io/js/pa-z0ezcBgw0icfE1LhxHPAm.js"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              "window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init();",
+          }}
+        />
+      </Head>
       <body className="bg-slate-100">
         <Main />
         <NextScript />
