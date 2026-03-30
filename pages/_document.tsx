8a707c7a50a40bfc79d50e78e2cd1b6502b585dd
Mike Warren <mikewarren@Mikes-MacBook-Pro.local>
2026-03-30 14:42:50 -0400

    Added cloudflare web analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,11 @@ export default function Document() {
               "var _paq = window._paq = window._paq || [];_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);(function(){var u='https://newspaper.matomo.cloud/';_paq.push(['setTrackerUrl', u+'matomo.php']);_paq.push(['setSiteId', '1']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.async=true;g.src='https://cdn.matomo.cloud/newspaper.matomo.cloud/matomo.js';s.parentNode.insertBefore(g,s);})();",
           }}
         />
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "bdfcd6bca45b4da1a8985526d4f16b90"}'
+        ></script>
       </Head>
       <body className="bg-slate-100">
         <Main />
