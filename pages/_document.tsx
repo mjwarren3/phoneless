8bea9b4c0641177fb2730666c6c5e9c13507fc6a
Mike Warren <mikewarren@Mikes-MacBook-Pro.local>
2026-03-30 14:20:43 -0400

    Added matomo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,12 @@ export default function Document() {
               "window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init();",
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              "var _paq = window._paq = window._paq || [];_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);(function(){var u='https://newspaper.matomo.cloud/';_paq.push(['setTrackerUrl', u+'matomo.php']);_paq.push(['setSiteId', '1']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.async=true;g.src='https://cdn.matomo.cloud/newspaper.matomo.cloud/matomo.js';s.parentNode.insertBefore(g,s);})();",
+          }}
+        />
       </Head>
       <body className="bg-slate-100">
         <Main />
