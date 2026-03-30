22ec7ebf367ff4f43c850c6856a0912d4848ce50
Mike Warren <mikewarren@Mikes-MacBook-Pro.local>
2026-03-30 13:27:58 -0400

    Installed plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        async
+        strategy="afterInteractive"
+        src="https://plausible.io/js/pa-glsletFAv3Li7iWWsD06g.js"
+      />
+      <Script id="plausible-init" strategy="afterInteractive">
+        {`window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init();`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
